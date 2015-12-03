89dde6c3a93b485c68c79edc535c5ce860231853
ArtiomCX75 <faa1192@gmail.com>
2015-12-03 17:02:34 +0300

    noth

--- src/audTests/TestRegAud.java
+++ src/audTests/TestRegAud.java
@@ -1,11 +1,11 @@
 package audTests;
 import org.junit.Test;
 
-import audHelpers.UserAudAud;
+import audHelpers.UserAud;
 public class TestRegAud extends TestBase{	
 	@Test
 	public void regAud() throws Exception{
-		UserAudAud Aud1 = new UserAudAud();
+		UserAud Aud1 = new UserAud();
 		Aud1.email = app.mail.setMail(Aud1.semiEmail);
 		app.audHelper.reg_aud(Aud1);
 	}	
--- src/audTests/TestRegPred.java
+++ src/audTests/TestRegPred.java
@@ -1,11 +1,11 @@
 package audTests;
 import org.junit.Test;
 
-import audHelpers.UserAudPred;
+import audHelpers.UserPred;
 public class TestRegPred extends TestBase {
 	@Test
 	public void regPred() throws Exception {
-		UserAudPred Pred1 = new UserAudPred();
+		UserPred Pred1 = new UserPred();
 		Pred1.email = app.mail.setMail(Pred1.semiEmail);
 		app.audHelper.reg_pred(Pred1);
 	}
